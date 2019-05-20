8eca08a47133075b638d13dc00ff939dad45deec
JunSeop Yoon <jay000111@naver.com>
2019-05-20 22:28:49 +0900

    1주차

---
+++ 1주차/PrintStars.java
@@ -0,0 +1,13 @@
+public class PrintStars /* 클래스의 이름이 파일의 이름과 같아야 하고 첫문자는 대문자이여야 한다. */
+{
+	public static void main(String[] args)/*메인함수이다*/
+	{
+		System.out.println("   *\n");
+		System.out.println("  ***\n");
+		System.out.println(" *****\n");
+		System.out.println("*******\n"); 
+		System.out.println(" *****\n");
+		System.out.println("  ***\n");
+		System.out.println("   *\n");
+	} 
+}
