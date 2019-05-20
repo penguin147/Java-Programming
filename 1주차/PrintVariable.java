12847a17863516bbc6028f541d45a29efab26490
JunSeop Yoon <jay000111@naver.com>
2019-05-20 22:32:04 +0900

    1주차

---
+++ 1주차/PrintVariable.java
@@ -0,0 +1,16 @@
+public class PrintVariable 
+{ 
+	public static void main(String[] args) { 
+		
+		int myNum = 5;//정수형 변수 myNum을 생성하고 5를 대입한다. 
+		float myFloatNum = 5.99f; //실수형 변수 myFloatNum을 선언하고 5.99를 대입한다.
+		char myLetter = 'D'; //문자형 변수 myLetter을 선언하고 D를 대입한다.
+		boolean myBool = true; //논리형 변수 myBool을 선언하고 true를 대입한다.
+		String myText = "Hello";//문자열을 저장하기 위해 String객체를 이용한다. Hello를 저장한다.
+		System.out.println(myNum);// myNum의 값을 출력한다. 개행도 실행한다.
+		System.out.println(myFloatNum);//myFloatNum의 값을 출력한다. 개행도 실행한다.
+		System.out.println(myLetter);//myLetter의 값을 출력한다. 개행도 실행한다.
+		System.out.println(myBool);//myBool의 값을 출력한다. 개행도 실행한다.
+		System.out.println(myText);//myText의 값을 출력한다. 개행도 실행한다.
+	}
+}
